86bb36b0653c952fc8c1e4839d4a118313d452ae
Chris Desmarais <desmarais_chris@hotmail.com>
2023-03-17 16:25:22 -0600

    Update to potluck stuff

--- problems/05_map.js
+++ problems/05_map.js
@@ -1,7 +1,8 @@
 const potluckSignUp = [
     {person: 'Chris', dish: 'cookies', type: 'dessert'},
     {person: 'Jen', dish: 'salad', type: 'side'},
-    {person: 'Danielle', dish: 'chilli', type: 'main'}
+    {person: 'Danielle', dish: 'chilli', type: 'main'}, 
+    {person: 'Greg', dish: 'pie', type: 'dessert'}
 ]
 
 // console log all the people who have signed up
